6cadae176637c475219e004736523a00b5c1079b
matthieudoliveira <matthieuoliv@umass.edu>
2017-02-23 18:53:16 -0500

    Added Glyphicon Toggle

--- build/js/grid.js
+++ build/js/grid.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
+	$('#documents .item').addClass('grid-group-item');
 	$('#list').click(function (event) {
 		console.log("list clicked");
 		event.preventDefault();
 		$('#documents .item').addClass('list-group-item');
+		$('#documents .item').removeClass('grid-group-item');
 	});
 	$('#grid').click(function (event) {
 		event.preventDefault();
