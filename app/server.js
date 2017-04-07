bb2f154a72eaf4dd8230db6798c1028b62b31f7e
MishaKanai <misha.a.kanai@gmail.com>
2017-04-07 01:09:24 -0400

    implement some server functions

--- app/server.js
+++ app/server.js
@@ -9,3 +9,117 @@ function emulateServerReturn(data, cb) {
     cb(data);
   }, 4);
 }
+
+// SYNC functions
+
+function removePasswordSync(userObj) {
+    if (userObj.hasOwnProperty('password')) {
+        delete userObj['password'];
+    }
+    return userObj;
+}
+
+function getUsersResolvedRefsSync(userId) {
+    var user = readDocument('users', userId);
+    user.collections =
+        user.collections.map(
+            (cid) => {
+                var collection = readDocument('collections', cid);
+                collection.documents =
+                    collection.documents.map(
+                        (did) => readDocument('documents', did)
+                    );
+                return collection;
+            }
+        );
+    user.documents =
+        user.documents.map(
+            (did) => readDocument('documents', did)
+        );
+    return removePasswordSync(user);
+}
+
+function addDocumentSync(title, text, timestamp) {
+    var doc = {
+        "title": title,
+        "text": text,
+        "timestamp": timestamp
+    };
+    doc = addDocument('documents', doc);
+    return doc;
+}
+
+// GET functions
+
+export function getCollections(userId, cb) {
+    var user = readDocument('users', userId);
+    var collections = user.collections.map(
+        (cid) => readDocument('collections', cid)
+    );
+    emulateServerReturn(collections, cb);
+}
+
+export function getUserDocuments(userId, cb) {
+    var user = readDocument('users', userId);
+    var documents = user.documents.map(
+        (did) => readDocument('documents', did)
+    );
+    emulateServerReturn(documents, cb);
+}
+
+export function getCollectionDocuments(collectionId, cb) {
+    var collection = readDocument('collections', collectionId);
+    var documents = collection.documents.map(
+        (did) => readDocment('documents', did)
+    );
+    emulateServerReturn(documents, cb);
+}
+
+export function getUserSettings(userId) {
+    var user = readDocument('users', userId);
+    emulateServerReturn(user.settings);
+}
+
+//falls back to user settings if document settings not set
+export function getDocumentSettings(userId, docId, cb) {
+    var doc = readDocument('documents', docId);
+    var settings;
+    if (doc.hasOwnProperty('settings'))
+        settings = doc.settings;
+    else
+        settings = readDocument('users', userId).settings;
+    emulateServerReturn(settings, cb);
+}
+
+// POST functions
+export function postUser(username, email, displayName, password, cb) {
+    var newUser = {
+        "username": username,
+        "email": email,
+        "displayName": displayName,
+        "password": password,
+        "settings": {
+            //default settings
+            "theme": "light",
+            "fontSize": 12
+        },
+        "collections": [],
+        "documents": []
+    };
+    newUser = removePasswordSync(addDocument('users', newUser));
+    emulateServerReturn(newUser, cb);
+}
+
+export function postDocumentToUser(userId, title, text, timestamp) {
+    var doc = addDocumentSync(title, text, timestamp);
+    var user = readDocument('users', userId);
+    user.documents.push(doc._id);
+    emulateServerReturn(removePasswordSync(writeDocument('users', user)));
+}
+
+export function postDocumentToCollection(collectionId, title, text, timestamp) {
+    var doc = addDocumentSync(title, text, timestamp);
+    var collection = readDocument('collections', collectionId);
+    collection.documents.push(doc._id);
+    emulateServerReturn(writeDocument('collections', collection));
+}
