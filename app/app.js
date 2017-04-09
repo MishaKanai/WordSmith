00d1d39945db9a3e941fa042da7da2d16a554054
MishaKanai <misha.a.kanai@gmail.com>
2017-04-09 17:35:34 -0400

    Revert "Merge branch 'master' of https://github.com/MishaKanai/WordSmith"
    This reverts commit a94c5fdb742eb612e960c2562df1c96675abec72, reversing
    changes made to 6f9a8d2f91980133ba59c68ccb759d5603924b00.

--- app/app.js
+++ app/app.js
@@ -1,22 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, browserHistory, Link } from 'react-router'
 
-// Each major browser view user interface must be imported.
+import NavBar from './components/Navbar';
 import Workspace from './components/Workspace';
 import Settings from './components/Settings';
 import SavedDocuments from './components/SavedDocuments';
-import EditorToolbar from './components/EditorToolbar';
+
+
+class WorkspacePage extends React.Component {
+    render() {
+        return (
+                <Workspace title='Document1' rhymeAPIprefix='http://rhymebrain.com/talk?function=getRhymes&word=' docId={this.props.params.id} />
+        );
+    }
+}
+
+class SavedDocumentsPage extends React.Component {
+    render() {
+        return (
+                <div>
+                <br/>
+                <SavedDocuments  userId={1}/>
+            </div>
+        );
+    }
+
+}
+
+
+
+/**
+ * The primary component in our application.
+ * The Router will give it different child Components as the user clicks
+ * around the application.
+ *
+ * This will contain the navbar
+ */
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+if (document.getElementById('app') !== null) {
+    ReactDOM.render((
+            <Router history={browserHistory}>
+              <Route path="/" component={App}>
+                <IndexRoute component={SavedDocumentsPage} />
+                <Route path="workspace/:id" component={WorkspacePage} />
+              </Route>
+            </Router>
+    ),document.getElementById('app'));
+}
+
+
+
 // For each view conditionally determine which view to display
 // depending on if the ID is present in the HTML.
-
-ReactDOM.render
-(
-  <EditorToolbar />, document.getElementById('docbar')
-);
-
-if (document.getElementById('workspace') !== null) {
+else if (document.getElementById('workspace') !== null) {
   ReactDOM.render(
-          <Workspace title='Document1' rhymeAPIprefix='http://rhymebrain.com/talk?function=getRhymes&word=' />,
+          <Workspace title='Document1'
+                rhymeAPIprefix='http://rhymebrain.com/talk?function=getRhymes&word='
+                synonymAPIprefix='http://api.datamuse.com/words?rel_syn='
+                definitionAPIprefix='http://api.datamuse.com/words?md=d&&sp='
+                docId='1' />,
     document.getElementById('workspace')
   );
 
@@ -26,7 +79,7 @@ if (document.getElementById('workspace') !== null) {
 );
 
 } else if (document.getElementById('documents') !== null) {
-  ReactDOM.render( <SavedDocuments  />,
+  ReactDOM.render( <SavedDocuments userId={1} />,
   document.getElementById('documents')
 );
 }
