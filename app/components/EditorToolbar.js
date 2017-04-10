785f16273a2f0d3dfd6bd39e3d59ef5649138e23
Taysiman Haque <thaque@umass.edu>
2017-04-09 22:38:31 -0400

    Implemented dropdown functionality for menus in EditorToolbar component.

--- app/components/EditorToolbar.js
+++ app/components/EditorToolbar.js
@@ -2,116 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// export default class EditorToolbar extends React.Component({
-//   render() {
-//     return (
-//       <div>
-//       // {/* Editor Toolbar */}
-//       <div className="navbar navbar-default" id="docbar">
-//         <div className="container">
-//           <div className="navbar-header pull-left">
-//             <ul id="docbar-list" className="nav navbar-nav pull-right">
-//               <li className="divider-vertical"> </li>
-//               <li><a href="#">File<span className="sr-only">(current)</span></a></li>
-//               <li className="divider-vertical"> </li>
-//               <li><a href="#">Edit<span className="sr-only">(current)</span></a></li>
-//               <li className="divider-vertical"> </li>
-//               <li><a href="#">View<span className="sr-only">(current)</span></a></li>
-//               <li className="divider-vertical"> </li>
-//               <li><a href="#">Share<span className="sr-only">(current)</span></a></li>
-//               <li className="divider-vertical"> </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     );
-//   }
-// })
-
-// class EditorToolbar extends React.Component
-// {
-//   constructor(props)
-//   {
-//     super(props)
-//     this.state=
-//     {
-//       open:false
-//     }
-//   }
-//
-//   _handleDropDown()
-//   {
-//     // console.log('dropdown')
-//    this.setState({
-//      open:!this.state.open
-//    })
-//   }
-//
-//   render()
-//   {
-//     return <div>
-//       <button type = "button" onClick={this._handleDropDown.bind(this)}>File</button>
-//         <div onClick={this._handleDropDown.bind(this)} className="navbar navbar-default" id="docbar">
-//           <div className="container">
-//             <div className="navbar-header pull-left">
-//               <ul id="docbar-list" className="nav navbar-nav pull-right">
-//                 <li clasmoodsName="divider-vertical"> </li>
-//                 <li><a href="#">File<span className="sr-only">(current)</span></a></li>
-//                 <li className="divider-vertical"> </li>
-//                 <li><a href="#">Edit<span className="sr-only">(current)</span></a></li>
-//                 <li className="divider-vertical"> </li>
-//                 <li><a href="#">View<span className="sr-only">(current)</span></a></li>
-//                 <li className="divider-vertical"> </li>
-//                 <li><a href="#">Share<span className="sr-only">(current)</span></a></li>
-//                 <li className="divider-vertical"> </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//   </div>;
-//   }
-// }
-//
-// React.render(<EditorToolbar />, document.getElementById('docbar'));
-// import ReactDOM from 'react-dom';
-// import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-//
-// export default class EditorToolbar extends React.Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.toggle = this.toggle.bind(this);
-//     this.state = {
-//       dropdownOpen: false
-//     };
-//   }
-//
-//   toggle() {
-//     this.setState({
-//       dropdownOpen: !this.state.dropdownOpen
-//     });
-//   }
-//
-//   render() {
-//   return (
-//     <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-//       <DropdownToggle caret>
-//         Dropdown
-//       </DropdownToggle>
-//       <DropdownMenu>
-//         <DropdownItem header>Header</DropdownItem>
-//         <DropdownItem disabled>Action</DropdownItem>
-//         <DropdownItem>Another Action</DropdownItem>
-//         <DropdownItem divider />
-//         <DropdownItem>Another Action</DropdownItem>
-//       </DropdownMenu>
-//     </Dropdown>
-//   );
-// }
-// }
-class EditorToolbar extends React.Component{
-// var EditorToolbar = React.createClass({
+export default class EditorToolbar extends React.Component{
   render() {
     return (
       <div>
@@ -121,13 +12,52 @@ class EditorToolbar extends React.Component{
           <div className="navbar-header pull-left">
             <ul id="docbar-list" className="nav navbar-nav pull-right">
               <li className="divider-vertical"> </li>
-              <li><a href="#">File<span className="sr-only">(current)</span></a></li>
+              <li>
+              <a href="#" data-toggle="dropdown" className="dropdown-toggle" >
+                File
+              </a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">New</a></li>
+                  <li><a href="#">Open</a></li>
+                  <li><a href="#">Save</a></li>
+                  <li><a href="#">Save As</a></li>
+                  <li><a href="#">Rename</a></li>
+                </ul>
+              </li>
               <li className="divider-vertical"> </li>
-              <li><a href="#">Edit<span className="sr-only">(current)</span></a></li>
+              <li>
+              <a href="#" data-toggle="dropdown" className="dropdown-toggle">
+                  Edit
+              </a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Undo</a></li>
+                  <li><a href="#">Redo</a></li>
+                  <li><a href="#">Select All</a></li>
+                  <li><a href="#">Find & Replace</a></li>
+                </ul>
+              </li>
               <li className="divider-vertical"> </li>
-              <li><a href="#">View<span className="sr-only">(current)</span></a></li>
+              <li>
+                <a href="#" data-toggle="dropdown" className="dropdown-toggle">
+                  View
+                </a>
+                <ul className= "dropdown-menu">
+                  <li><a href="#">Layout</a></li>
+                  <li><a href="#">Screen Size</a></li>
+                  <li><a href="#">Line Count</a></li>
+                </ul>
+              </li>
               <li className="divider-vertical"> </li>
-              <li><a href="#">Share<span className="sr-only">(current)</span></a></li>
+              <li>
+                <a href="#" data-toggle="dropdown" className="dropdown-toggle">
+                  Share
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">Download</a></li>
+                  <li><a href="#">Email</a></li>
+                  <li><a href="#">Export</a></li>
+                </ul>
+              </li>
               <li className="divider-vertical"> </li>
             </ul>
           </div>
