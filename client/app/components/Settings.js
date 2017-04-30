8f9ab02079dff9975786dce9f09eb2e12fc68806
Brian Silvey <blujay270@gmail.com>
2017-04-30 16:18:57 -0400

    cleaned up settings page, less content now

--- client/app/components/Settings.js
+++ client/app/components/Settings.js
@@ -6,100 +6,97 @@ import {SettingsUpdatePwModal} from './SettingsUpdatePwModal'
 import {getUserSettings, putUserSettings} from '../server'
 
 export default class Settings extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-            this.state = {
-                userSettings: {
-                    "email": "",
-                    "displayName": "",
-                    "password": "",
-                    "settings": {
-                        "theme": ""
-                    }
-                }
-            }
-
-        this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      userSettings: {
+        "email": "",
+        "displayName": "",
+        "password": "",
+        "settings": {
+          "theme": ""
+        }
+      }
     }
 
-    handleChange(id, value) {
-        putUserSettings(this.props.userId, id, value, (settings) => {
-            this.setState({
-                userSettings: settings
-            })
-        })
-    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-    componentDidMount() {
-        getUserSettings(this.props.userId, (settings) => {
-            this.setState({
-                userSettings: settings
-            })
-        })
-    }
+  handleChange(id, value) {
+    putUserSettings(this.props.userId, id, value, (settings) => {
+      this.setState({
+        userSettings: settings
+      })
+    })
+  }
 
-    render() {
-        var data = this.state.userSettings
-            return (
-                    <div>
+  componentDidMount() {
+    getUserSettings(this.props.userId, (settings) => {
+      this.setState({
+        userSettings: settings
+      })
+    })
+  }
 
-                    <div className="container">
-                    <div id="updatePassword" className="modal fade" role="dialog">
+  render() {
+    var data = this.state.userSettings
+    return (
+      <div>
 
-                    <div className="modal-dialog pwd-box">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal">&times;</button>
-                    <h4 className="modal-title">Update password</h4>
-                    </div>
-                    <SettingsUpdatePwModal onChange={(id, value) => this.handleChange(id, value)} id="password" oldPwd={data.password} />
-                    </div>
-                    </div>
-                    </div>
+      <div className="container">
+      <div id="updatePassword" className="modal fade" role="dialog">
 
-                    <div id="deleteAccount" className="modal fade" role="dialog">
-                    <div className="modal-dialog">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                    <button type="button" className="close" data-dismiss="modal">&times;</button>
-                    <h4 className="modal-title">Are you sure you want to delete your account?</h4>
-                    </div>
-                    <div className="modal-body">
-                    <button type="button" className="btn btn-danger modal-btn" data-dismiss="modal">Delete account</button>
-                    </div>
-                    </div>
-                    </div>
-                    </div>
-                    <div className="row">
-                    <div className="col-md-10 col-md-offset-1">
-                    <ul className="list-group">
-                    <li className="list-group-item bottom-border-only">
-                    <h1 className="text-center">Settings</h1>
-                    </li>
-                    <li className="list-group-item bottom-border-only">
-                    <h3 className="text-center">Wordsmith editor default settings</h3>
-                    </li>
-                    <SettingsElement subject="Theme"><SettingsListElements onChange={(id, value) => this.handleChange(id, value)} id="theme" elementArray={['Light', 'Dark', 'Gold', 'WordSmith']} selected={data.settings.theme} /></SettingsElement>
-                    <li className="list-group-item bottom-border-only">
-                    <h3 className="text-center">Account settings</h3>
-                    </li>
-                    <SettingsElement subject="Display name"><SettingsEditElement onChange={(id, value) => this.handleChange(id, value)} id="displayName" value={data.displayName} /></SettingsElement>
-                    <SettingsElement subject="Email"><SettingsEditElement onChange={(id, value) => this.handleChange(id, value)} id="email" value={data.email} /></SettingsElement>
-                    <SettingsElement subject="Password">
-                    <button type="button" className="btn btn-default right-element pull-right" data-toggle="modal" data-target="#updatePassword">Update</button>
-                    </SettingsElement>
-                    <li className="list-group-item bottom-border-only clearfix">
-                    <button type="button" className="btn btn-danger delete-btn" data-toggle="modal" data-target="#deleteAccount">
-                    Delete account
-                    </button>
-                    </li>
-                    </ul>
-                    </div>
-                    </div>
-                    </div>
+      <div className="modal-dialog pwd-box">
+      <div className="modal-content">
+      <div className="modal-header">
+      <button type="button" className="close" data-dismiss="modal">&times;</button>
+      <h4 className="modal-title">Update password</h4>
+      </div>
+      <SettingsUpdatePwModal onChange={(id, value) => this.handleChange(id, value)} id="password" oldPwd={data.password} />
+      </div>
+      </div>
+      </div>
 
-                    </div>
-                    )
-    }
+      <div id="deleteAccount" className="modal fade" role="dialog">
+      <div className="modal-dialog">
+      <div className="modal-content">
+      <div className="modal-header">
+      <button type="button" className="close" data-dismiss="modal">&times;</button>
+      <h4 className="modal-title">Are you sure you want to delete your account?</h4>
+      </div>
+      <div className="modal-body">
+      <button type="button" className="btn btn-danger modal-btn" data-dismiss="modal">Delete account</button>
+      </div>
+      </div>
+      </div>
+      </div>
+      <div className="row">
+      <div className="col-md-10 col-md-offset-1">
+      <ul className="list-group">
+      <li className="list-group-item bottom-border-only">
+      <h1 className="text-center">Settings</h1>
+      </li>
+      <SettingsElement subject="Theme"><SettingsListElements onChange={(id, value) => this.handleChange(id, value)} id="theme" elementArray={['Light', 'Dark', 'Gold', 'WordSmith']} selected={data.settings.theme} /></SettingsElement>
+      <li className="list-group-item bottom-border-only">
+      <h3 className="text-center">Account settings</h3>
+      </li>
+      <SettingsElement subject="Display name"><SettingsEditElement onChange={(id, value) => this.handleChange(id, value)} id="displayName" value={data.displayName} /></SettingsElement>
+      <SettingsElement subject="Email"><SettingsEditElement onChange={(id, value) => this.handleChange(id, value)} id="email" value={data.email} /></SettingsElement>
+      <SettingsElement subject="Password">
+      <button type="button" className="btn btn-default right-element pull-right" data-toggle="modal" data-target="#updatePassword">Update</button>
+      </SettingsElement>
+      <li className="list-group-item bottom-border-only clearfix">
+      <button type="button" className="btn btn-danger delete-btn" data-toggle="modal" data-target="#deleteAccount">
+      Delete account
+      </button>
+      </li>
+      </ul>
+      </div>
+      </div>
+      </div>
+
+      </div>
+    )
+  }
 }
